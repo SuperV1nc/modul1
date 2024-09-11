502bfcb0b289eae9b8740ad7de78233e34e246a7
Vinciant Andra Kaisarea <220711902@students.uajy.ac.id>
2024-09-11 15:21:47 +0700

    Update Tugas_Modul1_B_ANDRA_11902_.py

--- Tugas_Modul1_B_ANDRA_11902_.py
+++ Tugas_Modul1_B_ANDRA_11902_.py
@@ -4,10 +4,12 @@ import pickle
 import os
 
 #load model
-model_directory = r'D:\UAJY Kuliah Andra 220711902\Matkul\Semester 5\Pembelajaran Mesin dan Pembelajaran Mendalam B\Introduction to Machine Learning & MLOps with Python (Praktek)' ##diisi dengan path folder dimana file model berada
+#model_directory = r'D:\UAJY Kuliah Andra 220711902\Matkul\Semester 5\Pembelajaran Mesin dan Pembelajaran Mendalam B\Introduction to Machine Learning & MLOps with Python (Praktek)' ##diisi dengan path folder dimana file model berada
 
 # Gunakan os.path.join() untuk menggabungkan direktori dan file model pickle
-model_path = os.path.join(model_directory, 'rf_diabetes_model.pkl')
+#model_path = os.path.join(model_directory, 'rf_diabetes_model.pkl')
+
+model_path = 'rf_diabetes_model.pkl'
 
 # Periksa apakah file ada di direktori yang ditentukan
 if os.path.exists(model_path):
